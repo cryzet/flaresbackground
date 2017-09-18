478e5a29be19f4e8f67eb5f411f89e525d6a68aa
cryzet <bryanreyes9304@gmail.com>
2017-09-18 09:08:59 -0400

    Poniendo todo en orden

---
+++ README.md
@@ -0,0 +1,16 @@
+# Flare Background
+
+An iCloud Css background template
+
+## Authors
+
+* **Bryan Reyes** - *Initial work* - [Cryzet](https://github.com/Cryzet)
+* **Randy Téllez** - *Some bugs* - [Kronhyx](https://github.com/Kronhyx)
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
+
+## Demo
+
+[click here](https://cryzet.github.io/flarebacground/)
---
+++ js/bgflare.js
@@ -0,0 +1,18 @@
+window.onload = function() {
+    document.onmousemove = getPosition;
+};
+function getPosition(e){
+    var posicionX = -(e.clientX);
+    var posicionY = -(e.clientY);
+    var nodoCoordenadas = document.getElementById('coordenadas');
+    var capa1 = document.getElementById('cap_1');
+    var capa2 = document.getElementById('cap_2');
+    var capa3 = document.getElementById('cap_3');
+    var capa4 = document.getElementById('cap_4');
+    //nodoCoordenadas.innerHTML = 'Posicion x: ' + posicionX + ';  Posicion y: ' + posicionY;
+
+    capa1.setAttribute('style','position: absolute; top:'+posicionY/50+'px; left:'+posicionX/50+'px;');
+    capa2.setAttribute('style','position: absolute; top:'+posicionY/100+'px; left:'+posicionX/100+'px;');
+    capa3.setAttribute('style','position: absolute; top:'+posicionY/150+'px; left:'+posicionX/150+'px;');
+    capa4.setAttribute('style','position: absolute; top:'+posicionY/200+'px; left:'+posicionX/200+'px;');
+}
