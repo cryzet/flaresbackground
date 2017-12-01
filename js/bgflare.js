923344129b87d97e0c0551f423d3f24e3ce1761e
cryzet <bryanreyes9304@gmail.com>
2017-12-01 13:45:28 -0500

    Typo error

--- js/bgflare.js
+++ js/bgflare.js
@@ -10,14 +10,14 @@ function getPosition(e){
     var posicionY1 = (e.clientY) - documentH/2;
     var nodoCoordenadas = document.getElementById('coordenadas');
     var nodoCoordenadas2 = document.getElementById('coordenadas2');
-    var dimenciones = document.getElementById('dimensiones');
+    var dimensiones = document.getElementById('dimensiones');
     var capa1 = document.getElementById('cap_1');
     var capa2 = document.getElementById('cap_2');
     var capa3 = document.getElementById('cap_3');
     var capa4 = document.getElementById('cap_4');
     nodoCoordenadas.innerHTML = 'X: ' + posicionX + ';  Y: ' + posicionY;
     nodoCoordenadas2.innerHTML = 'X: ' + posicionX1 + ';  Y: ' + posicionY1;
-    dimenciones.innerHTML = 'Ancho: ' + documentW + ';  Alto: ' + documentH;
+    dimensiones.innerHTML = 'Ancho: ' + documentW + ';  Alto: ' + documentH;
 
     capa1.setAttribute('style','position: absolute; top:'+posicionY1+'px; left:'+posicionX1+'px;transition: 30s');
     capa2.setAttribute('style','position: absolute; top:'+posicionY1+'px; left:'+posicionX1+'px;transition: 40s');
