6ee8d2c1ce60112c5e4c09657167fa108bcc1ba4
cryzet <bryanreyes9304@gmail.com>
2017-12-01 14:01:57 -0500

    making_preview

--- js/bgflare.js
+++ js/bgflare.js
@@ -2,12 +2,12 @@ window.onload = function() {
     document.onmousemove = getPosition;
 };
 function getPosition(e){
-    var documentH = document.documentElement.clientHeight;
-    var documentW = document.documentElement.clientWidth;
-    var posicionX = (e.clientX);
-    var posicionY = (e.clientY);
-    var posicionX1 = (e.clientX) - documentW/2;
-    var posicionY1 = (e.clientY) - documentH/2;
+    var documentH = document.documentElement.clientHeight/2;
+    var documentW = document.documentElement.clientWidth/2;
+    var posicionX = (e.clientX)/2;
+    var posicionY = (e.clientY)/2;
+    var posicionX1 = posicionX - documentW/2;
+    var posicionY1 = posicionY - documentH/2;
     var nodoCoordenadas = document.getElementById('coordenadas');
     var nodoCoordenadas2 = document.getElementById('coordenadas2');
     var dimensiones = document.getElementById('dimensiones');
@@ -15,9 +15,9 @@ function getPosition(e){
     var capa2 = document.getElementById('cap_2');
     var capa3 = document.getElementById('cap_3');
     var capa4 = document.getElementById('cap_4');
-    nodoCoordenadas.innerHTML = 'X: ' + posicionX + ';  Y: ' + posicionY;
-    nodoCoordenadas2.innerHTML = 'X: ' + posicionX1 + ';  Y: ' + posicionY1;
-    dimensiones.innerHTML = 'Ancho: ' + documentW + ';  Alto: ' + documentH;
+    //nodoCoordenadas.innerHTML = 'X: ' + posicionX + ';  Y: ' + posicionY;
+    //nodoCoordenadas2.innerHTML = 'X: ' + posicionX1 + ';  Y: ' + posicionY1;
+    //dimensiones.innerHTML = 'Ancho: ' + documentW + ';  Alto: ' + documentH;
 
     capa1.setAttribute('style','position: absolute; top:'+posicionY1+'px; left:'+posicionX1+'px;transition: 30s');
     capa2.setAttribute('style','position: absolute; top:'+posicionY1+'px; left:'+posicionX1+'px;transition: 40s');
